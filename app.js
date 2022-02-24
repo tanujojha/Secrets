248255c764c445c6bc98504b2cd4c931a8a1a684
Tanuj Ojha <tanujojha2042@gmail.com>
2022-02-24 18:54:19 +0530

    Level 6, google OAuth2 and set up submit secrete page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
@@ -5,7 +6,8 @@ const bodyparser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
 const passportLocalMongoose = require('passport-local-mongoose');   // we dont need to require passport-local as passport-local-mongoose will be enough and will do the most of the work in a few lines of code
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findOrCreate');
 
 const app = express();
 
@@ -27,16 +29,44 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  googleId: String,
+  secrets: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+
+
+passport.use(new GoogleStrategy({             //using google Strategy and setting the options
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"      //same callback url given when creating OAuth consent screen
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);              //logs the profile of the user sent by google
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {       //finding or creating user with the googleId
+      return cb(err, user);
+    });
+  }
+));
 
 
 
@@ -44,6 +74,17 @@ app.get("/", function(req,res){
   res.render("home");
 });
 
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));     //authenticating at the /auth/google route
+
+
+  app.get('/auth/google/secrets',             //authenting at our local side
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect to secrets.
+      res.redirect('/secrets');
+    });
 
 
 app.get("/register", function(req,res){
@@ -96,13 +137,43 @@ app.post("/login", function(req,res){
 
 
 
-app.get("/secrets", function(req, res){
-  // console.log(req.isAuthenticated());    //returns boolean
-  if(req.isAuthenticated()){
-    res.render("secrets");
+app.get("/submit", function(req, res){
+  if(req.isAuthenticated()){            //checking for authentication fo the user; it returns boolean
+    res.render("submit");               //if true rendering the page; only authenticated users can post
   }else{
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  // console.log(req.body.secret);
+  const submittedSecret = req.body.secret
+  console.log(req.user);       //loging the user thats logged in and clicked the submit button
+
+  User.findById(req.user.id, function(err, foundUser){      //finding the user that posted their secrete to add in db
+    // console.log(foundUser);
+
+    if (!err){
+      foundUser.secrets = submittedSecret       //updating the secrets field of the user; note: it replaces the existing secrete if new secrete is posted by the same user
+      foundUser.save(function(){
+        res.redirect("/secrets")
+      })
+    }else{console.log(err);}
+
+  });
+
+
+});
+
+app.get("/secrets", function(req, res){
+
+  User.find({secrets: {$ne:null}}, function(err, foundUsers){       //finding all the users with the field secrets not  equal ($ne) to null
+    // console.log(foundUsers.secrets);       //logs array of users with matching conditions
+    if(!err){
+      res.render("secrets", {userWithSecrets: foundUsers});
+    }else{console.log(err);}
+  });
+
 });
 
 
